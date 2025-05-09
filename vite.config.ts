9391f46b9ca779669d6d718eb9d2d0d461f40029
Alex Sklar <sklarfox@gmail.com>
2025-05-09 13:18:48 -0700

    add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 import manifest from './src/manifest'
 
@@ -16,7 +17,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [crx({ manifest }), react()],
+    plugins: [crx({ manifest }), react(), tailwindcss()],
     legacy: {
       skipWebSocketTokenCheck: true,
     },
